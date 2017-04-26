87b29bcba37891c849a82d74b46d5cf7b8681cff
mathuramg <mmg9291@gmail.com>
2017-04-25 21:21:45 -0400

    add text accessibility

--- lib/app.tsx
+++ lib/app.tsx
@@ -153,6 +153,32 @@ export default class App extends PureComponent<AppProps, AppState> {
            ? <ErrorMessage {...this.state.lastError} />
            : null}
         </div>
+
+        <section
+        id="textOutput-content"
+        >
+          <h2> Text Output </h2>
+          <p
+            tabIndex="0"
+            role="main"
+            id="textOutput-content-summary"
+            aria-label="text output summary"
+          >
+          </p>
+          <table
+            id="textOutput-content-table"
+            summary="text output details"
+          >
+          </table>
+          <div
+            tabIndex="0"
+            role="main"
+            id="textOutput-content-details"
+            aria-label="text output details"
+          >
+          </div>
+        </section>
+
       </div>
     );
   }
